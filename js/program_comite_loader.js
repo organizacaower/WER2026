53aa5cfc8e47056d686c0f5193f7c175d414b9a2
Luminicen <julianadelleville@hotmail.com>
2026-03-12 10:10:30 -0300

    PC members to production

---
+++ js/program_comite_loader.js
@@ -0,0 +1,107 @@
+//traduciones xd
+const countryTranslations = {
+  BR: {
+    es: "Brasil",
+    en: "Brazil",
+    pt: "Brasil"
+  },
+  AR: {
+    es: "Argentina",
+    en: "Argentina",
+    pt: "Argentina"
+  },
+  ES: {
+    es: "España",
+    en: "Spain",
+    pt: "Espanha"
+  },
+  CL: {
+    es: "Chile",
+    en: "Chile",
+    pt: "Chile"
+  },
+  UY: {
+    es: "Uruguay",
+    en: "Uruguay",
+    pt: "Uruguai"
+  },
+  EC: {
+    es: "Ecuador",
+    en: "Ecuador",
+    pt: "Equador"
+  },
+  PT: {
+    es: "Portugal",
+    en: "Portugal",
+    pt: "Portugal"
+  },
+  PE: {
+    es: "Peru",
+    en: "Peru",
+    pt: "Peru"
+  },
+  DE: {
+    es: "Alemania",
+    en: "Germany",
+    pt: "Alemanha"
+  }
+};
+function getLanguageFromURL() {
+  const path = window.location.pathname.toLowerCase();
+
+  if (/\/es(\/|$)/.test(path)) return "es";
+  if (/\/pt(\/|$)/.test(path)) return "pt";
+  if (/\/en(\/|$)/.test(path)) return "en";
+
+  return "en"; // besto
+}
+function translateCountries(countryString,lang){
+
+  const codes = countryString.split(",").map(c => c.trim());
+
+  return codes
+    .map(code => countryTranslations[code]?.[lang] || code)
+    .join(" - ");
+}
+
+async function loadCommittee(){
+
+  const lang = getLanguageFromURL();
+  const list = document.getElementById("committee-list");
+
+  // id del body: RRT
+  const pageTrack = document.body.getAttribute("data-track");
+
+  // convertir a formato del JSON
+  const jsonTrack = "WER-" + pageTrack;
+  let cantidad  = 0
+  try{
+
+    const response = await fetch("../data/program_comite.json");
+    const committee = await response.json();
+
+    list.innerHTML = "";
+    
+    committee
+      .filter(person => person.track == jsonTrack)
+      .sort((a, b) => a.name.localeCompare(b.name))
+      .forEach(person => {
+
+
+        const countries = translateCountries(person.country,lang);
+        cantidad = cantidad + 1;
+
+        const li = document.createElement("li");
+        li.textContent =
+          `${person.name}, ${person.institution}, ${countries}`;
+
+        list.appendChild(li);
+      });
+
+  }catch(error){
+    console.error("Error cargando comité:",error);
+  }
+  console.log("Cantidad de miembros del comité: " + cantidad);
+}
+
+document.addEventListener("DOMContentLoaded",loadCommittee);
