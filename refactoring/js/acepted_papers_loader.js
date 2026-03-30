fc2fea19c31f0af67394e74ba55336fbb80f9b52
Luminicen <julianadelleville@hotmail.com>
2026-03-30 16:25:39 -0300

    Fix RT

--- refactoring/js/acepted_papers_loader.js
+++ refactoring/js/acepted_papers_loader.js
@@ -65,7 +65,7 @@ const trackNames = {
   }
 };
 
-const ordenTracks = ["RTT","JFT","MDT","TT","IT","SRTT","ST"];
+const ordenTracks = ["RRT","JFT","MDT","TT","IT","SRTT","ST"];
 
 fetch("../articulos_aceptados.json")
   .then(res => res.json())
