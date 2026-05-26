3dfc815b12b379942fa15ccd2e1c98fb4c8083f6
Luminicen <julianadelleville@hotmail.com>
2026-05-26 14:57:17 -0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -4,10 +4,6 @@
     attribution: '&copy; OpenStreetMap'
   }).addTo(map);
 
-  // Marcadores
-  L.marker([-34.9205, -57.9540]).addTo(map).bindPopup("UTN FRLP");
-  L.marker([-34.9179, -57.9516]).addTo(map).bindPopup("Facultad de Informática");
-  L.marker([-34.9211, -57.9545]).addTo(map).bindPopup("Plaza Moreno");
 
   // 🔥 FIX CLAVE
   setTimeout(function () {
@@ -15,12 +11,12 @@
   }, 200);
 
   // UTN FRLP
-  L.marker([-34.9205, -57.9540])
+  L.marker([-34.9042599, -57.9262465])
     .addTo(map)
     .bindPopup("UTN FRLP (WER)");
 
   // Facultad de Informática UNLP
-  L.marker([-34.9179, -57.9516])
+  L.marker([-34.90348, -57.9376558])
     .addTo(map)
     .bindPopup("Facultad de Informática UNLP");
 
