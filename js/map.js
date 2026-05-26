54ceed3d40aa569ebb5f1c4b68d141e5b5f29d00
Luminicen <julianadelleville@hotmail.com>
2026-05-26 14:40:44 -0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,12 +1,18 @@
- // Crear mapa centrado en La Plata
   var map = L.map('map').setView([-34.9214, -57.9544], 13);
 
-  // Capa base
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap'
   }).addTo(map);
 
-  // 📍 Marcadores
+  // Marcadores
+  L.marker([-34.9205, -57.9540]).addTo(map).bindPopup("UTN FRLP");
+  L.marker([-34.9179, -57.9516]).addTo(map).bindPopup("Facultad de Informática");
+  L.marker([-34.9211, -57.9545]).addTo(map).bindPopup("Plaza Moreno");
+
+  // 🔥 FIX CLAVE
+  setTimeout(function () {
+    map.invalidateSize();
+  }, 200);
 
   // UTN FRLP
   L.marker([-34.9205, -57.9540])
