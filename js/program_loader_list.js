e5a660bc4b4fe14c4a07bba9c05523f2807b5e2c
Luminicen <julianadelleville@hotmail.com>
2026-07-17 15:18:22 -0300

    Update program_loader_list.js

--- js/program_loader_list.js
+++ js/program_loader_list.js
@@ -30,7 +30,7 @@ function obtenerClavePrograma(paper) {
   const trackMap = {
     "RRT": "researchtrack",
     "IT": "industryandsrtt",
-    "IT": "industrytrack",
+    "IT1": "industrytrack",
     "JFT": "journalfirsttrack",
     "SRTT": "industryandsrtt",
     "TT": "tutorialtrack",
